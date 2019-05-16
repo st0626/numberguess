a4d33893bfe50f2624fdf168856bb2fcd4246577
Byron Tollison <st06266@gmail.com>
2019-05-15 21:38:57 -0500

    added comments

--- numberguessinggame/numberguessinggame/Program.cs
+++ numberguessinggame/numberguessinggame/Program.cs
@@ -6,19 +6,24 @@ namespace numberguessinggame
     {
         static void Main(string[] args)
         {
+            // start the game
             Console.WriteLine("Im thinking of a number between 1 and 10... can you guess what it is?");
 
+            //allow the computer to pick a random number
             Random random = new Random();
             int randomNumber = random.Next(1, 10);
 
+
+            //create a variable allowing the while loop to run
             bool playingGame = true;
 
+            //start game after user input
             while (playingGame)
             {
                 string userInput = Console.ReadLine();
                 int parsedInput = int.Parse(userInput);
 
-
+                // after user guesses, if it is wrong the game will continue to run
                 if (parsedInput != randomNumber)
                 {
                     Console.WriteLine("Sorry try again!");
